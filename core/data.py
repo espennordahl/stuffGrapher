c745d3966c70e0485fea0ad61f90690688d6bde6
Espen Nordahl <espen.nordahl@gmail.com>
2020-02-17 22:17:36 +0100

    Added some more connection rules

--- core/data.py
+++ core/data.py
@@ -8,7 +8,7 @@ class Data(Node):
         super(Data, self).__init__(match)
         inputAttr = InputAttribute("action", None, hidden=True)
         inputAttr.setConnectionCallback(self._checkInputConnection)
-        self.addAttribute(InputAttribute("action", None,hidden=True))
+        self.addAttribute(inputAttr)
 
     def visualName(self):
         if self.attributes["action"].value:
@@ -35,24 +35,61 @@ class GeoData(Data):
         return isinstance(connection.value, PublishGeoAction)
 
 
-
 class GeocacheData(Data):
     def __init__(self, match):
        super(GeocacheData, self).__init__(match)
+
+
+    def _checkInputConnection(self, connection):
+        if not isinstance(connection, OutputAttribute):
+            logging.debug("Connection not an OutputAttribute")
+            return False
+        from .actions import PublishGeoCacheAction
+        return isinstance(connection.value, PublishGeoCacheAction)
+
 
 class LookdevData(Data):
     def __init__(self, match):
        super(LookdevData, self).__init__(match)
 
+    def _checkInputConnection(self, connection):
+        if not isinstance(connection, OutputAttribute):
+            logging.debug("Connection not an OutputAttribute")
+            return False
+        from .actions import PublishLookdevAction
+        return isinstance(connection.value, PublishLookdevAction)
+
 class PlateData(Data):
     def __init__(self, match):
        super(PlateData, self).__init__(match)
+
+    def _checkInputConnection(self, connection):
+        if not isinstance(connection, OutputAttribute):
+            logging.debug("Connection not an OutputAttribute")
+            return False
+        from .actions import PublishPlateAction
+        return isinstance(connection.value, RenderPlateAction)
 
 class RenderData(Data):
     def __init__(self, match):
        super(RenderData, self).__init__(match)
 
+    def _checkInputConnection(self, connection):
+        if not isinstance(connection, OutputAttribute):
+            logging.debug("Connection not an OutputAttribute")
+            return False
+        from .actions import PublishRenderAction
+        return isinstance(connection.value, RenderAction)
+
 class ComprenderData(Data):
     def __init__(self, match):
        super(ComprenderData, self).__init__(match)
+
+    def _checkInputConnection(self, connection):
+        if not isinstance(connection, OutputAttribute):
+            logging.debug("Connection not an OutputAttribute")
+            return False
+        from .actions import PublishGeoAction
+        return isinstance(connection.value, ComprenderAction)
+
 
